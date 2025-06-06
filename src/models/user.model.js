411a55c2ca2f90211357211e671c38b67a9268a5
huynhkien <huynhhuukien2962@gmail.com>
2025-06-06 16:59:50 +0700

    Build model for rynan backend

---
+++ src/models/user.model.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const crypto = require('crypto');
+const userSchema = new mongoose.Schema({
+    name: {type: String, maxLength: 255, required: true},
+    password: {type: String, maxLength: 255, required: true},
+    email: {type: String, maxLength: 255, required: true},
+    phone: {type: String, maxLength: 255, required: true},
+    address: {
+        street: {type: String, maxLength: 255},
+        city: {type: String, maxLength: 100},
+        state: {type: String, maxLength: 100},
+        country: {type: String, maxLength: 100, default: 'Vietnam'},
+        zipCode: {type: String, maxLength: 20}
+    },
+    dateOfBirth: {type: Date,},
+    gender: {
+        type: String,
+        enum: ['male', 'female', 'other'],
+        lowercase: true
+    },
+    avatar: {
+        url: String, 
+        public_id: String
+    },
+    role: {
+        type: String, 
+        enum: [2000, 2002, 2004, 2006],
+        default: 2000
+    },
+    wishlist: [{type: mongoose.Types.ObjectId, ref: 'Product'}],
+    isBlocked: {type: Boolean, default: false},
+    refreshToken: {type: String},
+    passwordChangedAt: {type: String},
+    passwordResetToken: {type: String},
+    passwordResetExpires: {type: String},
+    registerToken: {type: String},
+},{
+    timestamps: true
+})
+
+// Xử lý hash password
+userSchema.pre('save', async function(next) {
+    if(!this.isModified('password')) {
+        return next();
+    }
+    const salt = bcrypt.genSaltSync(15);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+userSchema.methods = {
+    isCorrectPassword: async function(password) {
+        return await bcrypt.compare(password, this.password);
+    },
+    createPasswordChangeToken: function () {
+        const resetToken = crypto.randomBytes(32).toString('hex');
+        this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+        this.passwordResetExpires = Date.now() + 15*60*1000;
+        return resetToken
+    } 
+}
+module.exports = mongoose.model('User', userSchema);
