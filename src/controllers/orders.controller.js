a60bb38e41109c936740b3d59060e07102205fa5
huynhkien <huynhhuukien2962@gmail.com>
2025-06-09 17:11:15 +0700

    Build controllers for website

--- src/controllers/orders.controller.js
+++ src/controllers/orders.controller.js
@@ -15,7 +15,37 @@ const addOrder = asyncHandler(async(req, res) => {
         message: 'Thêm đơn hàng thành công'
     });
 })
-
+// Cập nhật trạng thái đơn hàng
+const updateOrder = asyncHandler(async(req, res) => {
+    const {oid} = req.params;
+    const response = await OrderService.updateOrder(oid, req.body.status);
+    if(!response){
+        return res.status(400).json({
+            success: false,
+            message: 'Cập nhật trạng thái đơn hàng không thành công'
+        });
+    }
+    return res.status(200).json({
+        success: true,
+        message: 'Cập nhật trạng thái đơn hàng thành công'
+    })
+});
+// Tìm đơn hàng theo id 
+const findOrderById = asyncHandler(async(req, res) => {
+    const {oid} = req.params;
+    const response = await OrderService.findOrderById(oid);
+    if(!response) {
+        return res.status(400).json({
+            success: false,
+            data: 'Không tìm thấy đơn hàng'
+        });
+    }
+    return res.status(200).json({
+        success: true,
+        data: response
+    });
+})
 module.exports = {
-    addOrder
+    addOrder,
+    updateOrder
 }
