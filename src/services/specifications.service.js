a00d3210ccc805fbb4644eef177070f65250bf22
huynhkien <huynhhuukien2962@gmail.com>
2025-06-24 16:49:07 +0700

    Update structure services

---
+++ src/services/specifications.service.js
@@ -0,0 +1,30 @@
+const Specification = require('../models/specification.model');
+const asyncHandler = require('express-async-handler');
+// thêm quy cách đóng gói
+const addSpecification = asyncHandler(async(data) => {
+    return await Specification.create(data);
+})
+// Cập nhật quy cách đóng gói
+const updateSpecification = asyncHandler(async(id, data) => {
+    return await Specification.findByIdAndUpdate(id, data, {new: true});
+})
+// Tìm quy cách đóng gói theo id
+const findSpecificationById = asyncHandler(async(id) => {
+    return await Specification.findById({_id: id});
+})
+// Tìm tất cả các quy cách đóng gói
+const findAllSpecification = asyncHandler(async() => {
+    return await Specification.find();
+})
+// Xóa quy cách đóng gói
+const deleteSpecification = asyncHandler(async (id) => {
+    console.log(id)
+    return await Specification.findByIdAndDelete(id);
+});
+module.exports = {
+    addSpecification,
+    updateSpecification,
+    findSpecificationById,
+    findAllSpecification,
+    deleteSpecification,
+}
