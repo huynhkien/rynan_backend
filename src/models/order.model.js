78e4c387d8da363d4e597289b4ec459cb0443b32
huynhkien <huynhhuukien2962@gmail.com>
2025-06-27 17:04:44 +0700

    Update structure models

--- src/models/order.model.js
+++ src/models/order.model.js
@@ -13,12 +13,33 @@ const OrderSchema = new mongoose.Schema({
         enum: ['Cancelled', 'Processing', 'Delivering', 'Received' ,'Succeed'],
         default: 'Processing'
     },
+    // Thông tin người đặt hàng
     orderBy: {type: mongoose.Types.ObjectId, ref: 'User'},
     location: {
         lat: {type: Number},
         lng: {type: Number}
     },
-    total: {type: Number}
+    total: {type: Number},
+    // Thông tin người nhận hàng => thông tin chỉ xuất hiện khi nhân viên thực hiện nhập đơn hàng 
+    recipient: {
+        name: String,
+        phone: String,
+        address: {
+            province: { code: Number, name: String },
+            district: { code: Number, name: String },
+            ward: { code: Number, name: String },
+            detail: String,       
+            addressAdd: String   
+        }
+    },
+    paymentMethod: {
+        type: String,
+        enum: ['COD', 'VNPay', 'Momo', 'ZaloPay'],
+        default: 'COD'
+    },
+    note: {type: String},
+    staff: {type: String},
+    expectedDeliveryDate: {type: Date}
 },{
     timestamps: true
 })
