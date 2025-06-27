b911788b367e111b00dca77a3e950e05dec7b68d
huynhkien <huynhhuukien2962@gmail.com>
2025-06-27 16:06:09 +0700

    Update contructure routes

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -2,6 +2,7 @@ const usersController = require('../controllers/users.controller');
 const express = require('express');
 const {verifyAccessToken, checkUserPermission} = require('../middlewares/auth');
 const router = express.Router();
+const uploader = require('../config/connectCloudinary');
 
 router.route('/').get(usersController.findAllUser);
 router.route('/register').post(usersController.register);
@@ -9,7 +10,8 @@ router.route('/final-register/:token').get(usersController.finalRegister);
 
 router.route('/:uid')
             .get(usersController.findUserById)
-            .put([verifyAccessToken, checkUserPermission],usersController.updateInfoByAdmin);
+            .put(usersController.updateInfoByAdmin)
+            .delete(usersController.deleteUser);
 
 router.route('/find-user').get(usersController.findUserByToken);
 
@@ -20,6 +22,7 @@ router.route('/forgot-password').get(usersController.forgotPassword);
 router.route('/reset-password').put(usersController.resetPassword);
 
 router.route('/update-user').put(usersController.updateInfoByUser);
+router.route('/add-user').post(uploader.single('avatar'), usersController.addUserByAdmin);
 router.route('/update-address').put(usersController.updateAddress);
 
 module.exports = router;
