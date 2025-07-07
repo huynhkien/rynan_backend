5149020164d05980e3e5d87022cf2fa3262ec9e9
huynhkien <huynhhuukien2962@gmail.com>
2025-07-07 17:10:52 +0700

    Update structure services

--- src/services/receipts.service.js
+++ src/services/receipts.service.js
@@ -6,20 +6,18 @@ const addReceipt = asyncHandler(async(data)=> {
 });
 // Cập nhật thông tin phiếu 
 const updateReceipt = asyncHandler(async(rid, data) => {
-    const receipt = findReceiptById(rid);
-    let updateData = {...data};
-    if(!receipt) throw new Error('Không tìm thấy thông tin phiếu');
-    if(receipt.typeReceipt === 'export' && data.typeReceipt === 'import'){
-        updateData.exportedTo = null;
-    }
-    return await Receipt.findByIdAnUpdate(rid, updateData, {new: true});
+    return await Receipt.findByIdAndUpdate(rid, data, {new: true});
+});
+// Tìm thông tin phiếu theo id
+const findReceiptById = asyncHandler(async(id) => {
+    return await Receipt.findById({_id: id});
 });
 // Cập nhật thông tin sản phẩm trong phiếu
-const updateProductReceipt = asyncHandler(async({rid, pid, data}) => {
-    const receipt = findReceiptById(rid);
+const updateProductReceipt = asyncHandler(async(rid, pid, data) => {
+    const receipt = await Receipt.findById(rid);
     if(!receipt) throw new Error('Không tìm thấy thông tin phiếu');
-    const productIndex = receipt.products.findIndex(item => item._id.toString() === pid);
-    if(productIndex !== -1) throw new Error('Không tìm thấy sản phẩm trong phiếu');
+    const productIndex = receipt.products.findIndex(item => item.pid.toString() === pid);
+    if(productIndex === -1) throw new Error('Không tìm thấy sản phẩm trong phiếu');
     const updateData = {
         ...receipt.products[productIndex].toObject(),
         ...data
@@ -27,17 +25,34 @@ const updateProductReceipt = asyncHandler(async({rid, pid, data}) => {
     receipt.products[productIndex] = updateData;
     return receipt.save();
 });
-// Tìm thông tin phiếu theo id
-const findReceiptById = asyncHandler(async(id) => {
-    return await Receipt.findById({_id: id});
+
+// Cập nhật thông tin nguyên liêuk trong phiếu
+const updateMaterialReceipt = asyncHandler(async(rid, mid, data) => {
+    if(!rid) throw new Error('Không tìm thấy id');
+    console.log(rid);
+    const receipt = await Receipt.findById(rid);
+    if(!receipt) throw new Error('Không tìm thấy');
+    if (!receipt.materials.length < 0) {
+    throw new Error('Không tìm thấy danh sách nguyên vật liệu trong phiếu');
+    }
+    const materialIndex = receipt?.materials.findIndex(item => item.mid.toString() === mid);
+    if (materialIndex === -1) throw new Error('Không tìm thấy sản phẩm trong phiếu');
+    const updateData = {
+        ...receipt.materials[materialIndex].toObject(),
+        ...data
+    }
+    receipt.materials[materialIndex] = updateData;
+    receipt.total = receipt?.materials?.reduce((sum, item) => sum + (item?.price * item?.quantity || 0), 0)
+    return receipt.save();
 });
+
 // Tìm thông tin tất cả các phiếu
 const findAllReceipt = asyncHandler(async() => {
     return await Receipt.find();
 });
 // Xóa thông tin phiếu
 const deleteReceipt = asyncHandler(async(id) => {
-    return await Receipt.findByIdAnDelete({_id: id});
+    return await Receipt.findByIdAndDelete({_id: id});
 });
 // Xóa thông tin sản phẩm trong phiếu
 const deleteProductReceipt = asyncHandler(async(rid, pid) => {
@@ -47,12 +62,22 @@ const deleteProductReceipt = asyncHandler(async(rid, pid) => {
         {new: true}
     )
 })
+// Xóa thông tin nguyên liêu trong phiếu
+const deleteMaterialReceipt = asyncHandler(async(rid, mid) => {
+    return await Receipt.findByIdAndUpdate(
+        rid,
+        {$pull: {materials: {mid: mid}}},
+        {new: true}
+    )
+})
 module.exports = {
     addReceipt,
     updateReceipt,
     updateProductReceipt,
+    updateMaterialReceipt,
     findReceiptById,
     findAllReceipt,
     deleteReceipt,
-    deleteProductReceipt
+    deleteProductReceipt,
+    deleteMaterialReceipt
 }
