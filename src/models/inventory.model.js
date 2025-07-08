26c05006b45759717820dd20452877359f83a67c
huynhkien <huynhhuukien2962@gmail.com>
2025-07-08 17:04:24 +0700

    Update structure models

--- src/models/inventory.model.js
+++ src/models/inventory.model.js
@@ -4,19 +4,53 @@ const InventorySchema = new mongoose.Schema({
     productId: {
         type: mongoose.Types.ObjectId, 
         ref: 'Product',
-        required: true
     },
+    materialId: {
+        type: mongoose.Types.ObjectId, 
+        ref: 'Material',
+    },
+    type: {type: String},
     currentStock: {type: Number, default: 0}, // Số lượng hiện tại
-    reservedStock: {type: Number, default: 0}, // Hàng đã đặt chỗ
-    availableStock: {type: Number, default: 0}, // Hàng có thể bán
-    committedStock: {type: Number, default: 0, min: 0}, // Đã cam kết bán
+    approvedBy: {type: String},
+    approvalHistory: [{
+        approvedBy: {
+            type: mongoose.Types.ObjectId, 
+            ref: 'User',
+            required: true
+        },
+        approvedAt: {
+            type: Date,
+            default: Date.now
+        },
+        action: {
+            type: String,
+            enum: ['created', 'updated'],
+            required: true
+        },
+        quantityChange: {
+            type: Number,
+            default: 0
+        },
+        previousStock: {
+            type: Number,
+            default: 0
+        },
+        newStock: {
+            type: Number,
+            default: 0
+        },
+        notes: String
+    }],
     minStock: {type: Number, default: 50}, // Mức tồn kho tối thiểu
-    maxStock: {type: Number}, // Mức tồn kho tối đa
-    location: {type: String}, // Vị trí trong kho
+    location: {
+        shelf: String,
+        positionCode: String
+    }, // Vị trí trong kho
     lastUpdated: {type: Date, default: Date.now}
 }, {
     timestamps: true
 });
 
+InventorySchema.index({ productId: 1, materialId: 1 }, { unique: true });
 
 module.exports = mongoose.model('Inventory', InventorySchema);
