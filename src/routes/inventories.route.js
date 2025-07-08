8bdf8c94888c63e907a9e5e0d65159a84a51372e
huynhkien <huynhhuukien2962@gmail.com>
2025-07-08 17:04:36 +0700

    Update structure routes

--- src/routes/inventories.route.js
+++ src/routes/inventories.route.js
@@ -3,6 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 
-router.route('/').get(inventoriesController.findAllInventory);
+router.route('/').get(inventoriesController.findAllInventory)
+                 .post(inventoriesController.addInventory);
+                 
+router.route('/remove-inventory').put(inventoriesController.removeInventory);
 
 module.exports = router;
