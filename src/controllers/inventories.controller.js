309fd9213204a6894214060758044684930f8637
huynhkien <huynhhuukien2962@gmail.com>
2025-07-08 17:04:13 +0700

    Update structure controllers

--- src/controllers/inventories.controller.js
+++ src/controllers/inventories.controller.js
@@ -1,6 +1,34 @@
 const InventoryService = require('../services/inventories.service');
 const asyncHandler = require('express-async-handler');
 
+// Thêm sản phẩm vào kho
+const addInventory = asyncHandler(async(req, res) => {
+    const response = await InventoryService.addInventory(req.body);
+    if(!response) {
+        return res.status(400).json({
+            success: false,
+            message: 'Lỗi khi thực hiện quá trình nhập kho'
+        });
+    }
+    return res.status(200).json({
+            success: true,
+            message: 'Nhập kho thành công'
+        });
+});
+// Xuất kho
+const removeInventory = asyncHandler(async(req, res) => {
+    const response = await InventoryService.removeInventory(req.body);
+    if(!response) {
+        return res.status(400).json({
+            success: false,
+            message: 'Lỗi khi thực hiện quá trình xuất kho'
+        });
+    }
+    return res.status(200).json({
+            success: true,
+            message: 'Xuất kho thành công'
+        });
+})
 // Lấy tất cả các thông tin 
 const findAllInventory = asyncHandler(async(req, res) => {
     const response = await InventoryService.findAllInventory();
@@ -17,5 +45,7 @@ const findAllInventory = asyncHandler(async(req, res) => {
 });
 
 module.exports = {
-    findAllInventory
+    findAllInventory,
+    addInventory,
+    removeInventory
 }
