119c39ca65ce2b0126f09953cd84b9ad22dc8f74
huynhkien <huynhhuukien2962@gmail.com>
2025-07-12 17:17:05 +0700

    Update structure routes

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -26,5 +26,5 @@ router.route('/reset-password').put(usersController.resetPassword);
 router.route('/update-user').put(usersController.updateInfoByUser);
 router.route('/add-user').post(uploader.single('avatar'), usersController.addUserByAdmin);
 router.route('/update-address').put(usersController.updateAddress);
-
+router.route('/check-mail').post(usersController.checkMail);
 module.exports = router;
