19f564c229cfa0db070c3a6287728b009b70f815
huynhkien <huynhhuukien2962@gmail.com>
2025-07-14 08:02:41 +0700

    Update structure config

--- src/config/config.js
+++ src/config/config.js
@@ -7,6 +7,12 @@ const config = {
     }, 
     abstract_api_key: {
         key: process.env.ABSTRACT_API_KEY
+    }, 
+    email_name: {
+        value: process.env.EMAIL_NAME
+    }, 
+    email_app_password: {
+        password: process.env.EMAIL_APP_PASSWORD
     }
 }
 
