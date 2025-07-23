56b73bc5034d0bff5d729c7167a8b66a7e83866f
huynhkien <huynhhuukien2962@gmail.com>
2025-07-23 12:37:34 +0700

    Update structure config

--- src/config/config.js
+++ src/config/config.js
@@ -13,7 +13,20 @@ const config = {
     }, 
     email_app_password: {
         password: process.env.EMAIL_APP_PASSWORD
-    }
+    },
+    vnp_TmnCode: {
+        code: process.env.VNP_TMNCODE
+    },
+    vnp_HashSecret: {
+        secret: process.env.VNP_HASH_SECRET
+    },
+    vnp_Url: {
+        url: process.env.VNP_URL
+    },
+    vnp_Url_Return: {
+        url: process.env.VNP_RETURN_URL
+    },
+
 }
 
 module.exports = config;
